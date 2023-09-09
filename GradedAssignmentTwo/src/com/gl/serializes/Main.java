13fe833eec7dd350c3a125867f20d376a0901da7
Rangapanditi <125783848+Rangapanditi@users.noreply.github.com>
2023-09-09 19:18:52 +0530

    Add files via upload

---
+++ GradedAssignmentTwo/src/com/gl/serializes/Main.java
@@ -0,0 +1,45 @@
+
+package com.gl.serializes;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ProjectSerializer projectSerializer = new ProjectSerializer();
+
+        Project project1 = new Project("P1", "Music Synthesizer", 23);
+        Project project2 = new Project("P2", "Vehicle Movement Tracker", 13);
+        Project project3 = new Project("P3", "Liquid Viscosity Finder", 15);
+
+        Employee e1 = new Employee("E001", "Harsha", "9383993933", "RTNagar", 1000);
+        Employee e2 = new Employee("E002", "Harish", "9354693933", "Jayanagar", 2000);
+        Employee e3 = new Employee("E003", "Meenal", "9383976833", "Malleswaram", 1500);
+
+        projectSerializer.projectMap.put(project1, new ArrayList<>(Arrays.asList(e1, e2, e3)));
+
+        Employee e4 = new Employee("E004", "Sundar", "9334593933", "Vijayanagar", 3000);
+        Employee e5 = new Employee("E005", "Suman", "9383678933", "Indiranagar", 2000);
+        Employee e6 = new Employee("E006", "Suma", "9385493933", "KRPuram", 1750);
+
+        projectSerializer.projectMap.put(project2, new ArrayList<>(Arrays.asList(e4, e5, e6)));
+
+        Employee e7 = new Employee("E007", "Chitra", "9383978933", "Koramangala", 4000);
+        Employee e8 = new Employee("E008", "Suraj", "9383992133", "Malleswaram", 3000);
+        Employee e9 = new Employee("E009", "Manu", "9345193933", "RTNagar", 2000);
+
+        projectSerializer.projectMap.put(project3, new ArrayList<>(Arrays.asList(e7, e8, e9)));
+
+        projectSerializer.serializeProjectDetails(projectSerializer.projectMap);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        projectSerializer.deserializeProjectDetails();
+    }
+}
+
---
+++ GradedAssignmentTwo/src/com/gl/serializes/ProjectSerializer.java
@@ -0,0 +1,58 @@
+package com.gl.serializes;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProjectSerializer {
+    Map<Project, ArrayList<Employee>> projectMap = new HashMap<>();
+
+    public void serializeProjectDetails(Map<Project, ArrayList<Employee>> projectMap) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("project_data.ser"))) {
+            oos.writeObject(projectMap);
+            System.out.println("Serialized map of project successfully...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deserializeProjectDetails() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("project_data.ser"))) {
+            Map<Project, ArrayList<Employee>> deserializedMap = (Map<Project, ArrayList<Employee>>) ois.readObject();
+
+            for (Map.Entry<Project, ArrayList<Employee>> entry : deserializedMap.entrySet()) {
+                Project project = entry.getKey();
+                ArrayList<Employee> employees = entry.getValue();
+
+                System.out.println("DeSerialized Data:");
+                System.out.println("The Project " + project + " Has the following Employees");
+                System.out.println("Employees .............");
+                System.out.print("[");
+                for (int i = 0; i < employees.size(); i++) {
+                    System.out.print(employees.get(i));
+                    if (i < employees.size() - 1) {
+                        System.out.print(", ");
+                    }
+                }
+                System.out.println("]");
+                System.out.println();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+	public Map<Project, ArrayList<Employee>> getProjectMap() {
+		return projectMap;
+	}
+
+	public void setProjectMap(Map<Project, ArrayList<Employee>> projectMap) {
+		this.projectMap = projectMap;
+	}
+}
